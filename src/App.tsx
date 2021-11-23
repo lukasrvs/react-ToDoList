c4e4942175a0f9543071fe9497e385b8a988ef50
lukasrvs <lucas_antonio.v@hotmail.com>
2021-11-22 21:37:55 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import * as Component from './App.styles';
+import { Item } from './types/Item';
+import { ListItem } from './components/ListItem';
+import { AddArea } from './components/AddArea';
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar o pão', done: false },
+    { id: 2, name: 'Abastecer o carro', done: true },
+  ]);
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length+1,
+      name: taskName,
+       done: false,
+    });
+    setList(newList);
+  }
+  const handleTaskChange = (id: number, done: boolean) =>{
+    let newList = [...list];
+    for(let i in newList){
+      if(newList[i].id === id){
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
+  return(
+    <Component.Container>
+      <Component.Area>
+        <Component.Header>To-do list</Component.Header>
+        <AddArea onEnter={handleAddTask} />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        {list.map((item, index)=>(
+          <ListItem 
+            key={index} 
+            item={item} 
+            onChange={handleTaskChange}
+          />
+        ))}
+
+      </Component.Area>
+    </Component.Container>
   );
 }
-
 export default App;
---
+++ src/components/AddArea/index.tsx
@@ -0,0 +1,26 @@
+import { useState, KeyboardEvent } from 'react';
+import * as Component from './styles';
+type Props = {
+    onEnter: (taskName: string) => void
+}
+export const AddArea = ({ onEnter}: Props) => {
+    const [inputText, setInputText] = useState('');
+    const handleKeyUp = (e: KeyboardEvent) => {
+        if(e.code === 'Enter' && inputText !== ''){
+            onEnter(inputText);
+            setInputText('');
+        }
+    }
+    return (
+        <Component.Container>
+            <div className="image">➕</div>
+            <input 
+                type="text" 
+                placeholder="add a new task" 
+                value={inputText} 
+                onChange={e=>setInputText(e.target.value)}
+                onKeyUp={handleKeyUp}
+            />
+        </Component.Container>
+    );
+}
